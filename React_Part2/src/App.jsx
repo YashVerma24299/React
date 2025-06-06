24f0de51ff027221839bf46d956cbb45ec865986
Yash <ritikverma23@gmail.com>
2025-06-06 09:31:45 +0530

    Perform Product component Styling.

---
+++ React_Part2/src/App.jsx
@@ -0,0 +1,14 @@
+import ProductTab from './ProductTab.jsx';
+import './App.css';
+
+function App() {
+    return(
+        <>
+            <div className="App">
+                <ProductTab/>
+            </div>
+        </>
+    )   
+}
+
+export default App;
