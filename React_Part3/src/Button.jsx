c3b90fab2b3d047911b23472488f5c856d697abd
Yash <ritikverma23@gmail.com>
2025-06-06 17:30:17 +0530

    Unserstand the basic of Event Object.

--- React_Part3/src/Button.jsx
+++ React_Part3/src/Button.jsx
@@ -1,5 +1,6 @@
-function printHello(){
+function printHello(event){
     console.log("Hello!");
+    console.log(event); //see in console
 }
 function printHover(){
     console.log("Hover!");
@@ -7,6 +8,11 @@ function printHover(){
 function handledblclick(){
     console.log("dblClick!");
 }
+function handleFormSubmit(event){
+    // event.preventDefault();  // run with or without (IMPORTANT) 
+    console.log("Form was submitted!");
+}
+
 
 export default function Button(){
     return(
@@ -14,6 +20,11 @@ export default function Button(){
             <button onClick={printHello}>Click me</button>
             <p onMouseOver={printHover}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, officiis. Rem culpa, eos repudiandae cumque ex quidem repellendus mollitia corporis nesciunt non quasi quibusdam labore iste, explicabo facilis iusto incidunt!</p>
             <button onDoubleClick={handledblclick}>Double Click</button>
+
+            <form>
+                <input type="text" placeholder="Enter something" />
+                <button onClick={handleFormSubmit}>Submit</button>
+            </form>
         </div>
     )
 }
