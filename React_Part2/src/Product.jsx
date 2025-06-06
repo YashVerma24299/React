9240fe322d31af9ae845119c02c4ae2d24712591
Yash <ritikverma23@gmail.com>
2025-06-06 10:19:07 +0530

    Learn and implement passing arrays to props and display them.

--- React_Part2/src/Product.jsx
+++ React_Part2/src/Product.jsx
@@ -1,11 +1,14 @@
 import './Product.css';
 
-function Product({title, price=10}) {
-    console.log(title);
+function Product({title, price=10, feature,  feature2, feature3}) {
+    const list = feature3.map((featureeee) => <li>{featureeee}</li>)
     return(
         <div className="Product">  
             <h3>{title}</h3>
             <p>Product Price: {price/2}</p>
+            <p>FEATURE1: {feature}</p>
+            <p>FEATURE2:{feature2}</p>
+            <p>FEATURE3:{list}</p>
         </div>
     )   
 }
--- React_Part2/src/ProductTab.jsx
+++ React_Part2/src/ProductTab.jsx
@@ -1,11 +1,14 @@
 import Product from './Product.jsx';
 
 function ProductTab() {
+    let options = ["hi-tech", "durable", "fast" ]
+    let options2 = [<li>hi-tech</li>, <li>durable</li>, <li>fast</li> ]
+    let options3 = ["hi-tech", "durable", "fast" ]
     return(
         <>
-            <Product title="Phone" price={30000}/>
-            <Product title="Laptop" price={50000}/>
-            <Product title="pen"/>
+            <Product title="Phone" price={30000} feature={options} feature2={options2} feature3={options3}/>
+            <Product title="Laptop" price={50000} feature={options} feature2={options2} feature3={options3}/>
+            <Product title="pen" feature={options} feature2={options2} feature3={options3}/>
         </>
     )   
 }
