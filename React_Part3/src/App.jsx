1c439e2fd7eeadc8a6af4e461b8b71ce996350e5
Yash <ritikverma23@gmail.com>
2025-06-06 17:13:28 +0530

    Implement handle click-listeners.

---
+++ React_Part3/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from'./Button.jsx';
+import './App.css';
+
+
+function App() {
+    return(
+        <>
+            <div className="App">
+                <Button/>
+            </div>
+        </>
+    )   
+}
+
+export default App;
