228163c1f401482404c43459250fe1bef14f4493
Yash <ritikverma23@gmail.com>
2025-06-06 17:21:25 +0530

    Perform more click and non-click events.

--- React_Part3/src/Button.jsx
+++ React_Part3/src/Button.jsx
@@ -1,11 +1,19 @@
 function printHello(){
     console.log("Hello!");
 }
+function printHover(){
+    console.log("Hover!");
+}
+function handledblclick(){
+    console.log("dblClick!");
+}
 
 export default function Button(){
     return(
         <div className="button">
             <button onClick={printHello}>Click me</button>
+            <p onMouseOver={printHover}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, officiis. Rem culpa, eos repudiandae cumque ex quidem repellendus mollitia corporis nesciunt non quasi quibusdam labore iste, explicabo facilis iusto incidunt!</p>
+            <button onDoubleClick={handledblclick}>Double Click</button>
         </div>
     )
 }
